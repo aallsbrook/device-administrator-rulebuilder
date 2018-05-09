4c3c5b4b6b100f6da4c72aa7259806fe4c9270eb
Aaron Allsbrook <aallsbrook@clearblade.com>
2018-05-09 17:02:56 -0500

    Asset created by aallsbrook

---
+++ code/libraries/devadm_constants/devadm_constants.js
@@ -0,0 +1,6 @@
+var devadm_platform_url = "https://staging.clearblade.com";
+
+var devadm_developer_email = "";
+var devadm_developer_password = "";
+
+var devadm_validTypes = ["string", "int", "bigint", "float", "double", "blob", "uuid", "timestamp", "boolean"];
