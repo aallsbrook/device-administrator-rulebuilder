3abc9fd73d94db7e702727e41b96141c109a868b
Aaron Allsbrook <aallsbrook@clearblade.com>
2018-05-09 17:03:00 -0500

    Asset created by aallsbrook

---
+++ code/services/devadmGetAllUsers/devadmGetAllUsers.js
@@ -0,0 +1,11 @@
+function devadmGetAllUsers(req, resp){
+    ClearBlade.init({request:req});
+    var user = ClearBlade.User();
+    // Right Now sending all possible users, but one might want to restrict later on
+    user.allUsers(ClearBlade.Query({}), function(err, r){
+        if(err){
+            resp.error(r);
+        }
+        resp.success(r);
+    })  
+}
